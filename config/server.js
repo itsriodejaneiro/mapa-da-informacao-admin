98b91fab4de32ebdfff2deddd46c9cd47e866827
vitusalis <victorsalles1997@gmail.com>
2021-08-13 09:40:20 -0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aaa3573a8d94901a094d215d62036e97'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e983cbf4-e7b4-4e58-8559-769d285a1412'
+};
