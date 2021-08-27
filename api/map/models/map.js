1943694ef438e81263881c9295e03cfbb0b6f30c
vitusalis <victorsalles1997@gmail.com>
2021-08-27 15:55:05 -0300

    Set slug on Map.node_groups

--- api/map/models/map.js
+++ api/map/models/map.js
@@ -5,4 +5,53 @@
  * to customize this model
  */
 
-module.exports = {};
+
+async function setSlug(map) {
+    // Create slugs for nodes for better Map admin usage
+
+    async function getNodeTitle(id) {
+        if (id) {
+            const nodeFound = await strapi.services.node.findOne({ 'id': id })
+            if (nodeFound) {
+                return nodeFound.title
+            }
+        }
+        return null
+    }
+
+
+    await Promise.all(
+        map.node_groups.map(async node => {
+
+            const currentNode = await getNodeTitle(node.node)
+            let nextNodes = []
+
+            // get all nodes names and concat
+            if (node.next_nodes){
+                for (let nextNode of node.next_nodes) {
+                    const nodeTitle = await getNodeTitle(nextNode)
+                    if (nodeTitle) {
+                        nextNodes.push(nodeTitle)
+                    }
+                }
+            }
+            const nextNode = nextNodes.join(', ')
+
+            node.slug = [currentNode, nextNode]
+                .filter(element => element && element.length) // filter out null fields
+                .join(': ')
+        })
+    )
+    return map
+}
+
+module.exports = {
+    lifecycles: {
+        beforeCreate: async (map) => {
+            map = await setSlug(map)
+        },
+        beforeUpdate: async (params, map) => {
+            map = await setSlug(map)
+        },
+    },
+};
