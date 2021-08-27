1f9745005a0598659799aa837152f13eb48d41f8
vitusalis <victorsalles1997@gmail.com>
2021-08-27 18:24:52 -0300

    Replace node-component with category-component at Map model

--- api/map/models/map.js
+++ api/map/models/map.js
@@ -9,6 +9,15 @@
 async function setSlug(map) {
     // Create slugs for nodes for better Map admin usage
 
+    async function getCategoryTitle(id) {
+        if (id) {
+            const category = await strapi.services.category.findOne({ 'id': id })
+            if (category) {
+                return category.title
+            }
+        }
+        return null
+    }
     async function getNodeTitle(id) {
         if (id) {
             const nodeFound = await strapi.services.node.findOne({ 'id': id })
@@ -21,26 +30,38 @@ async function setSlug(map) {
 
 
     await Promise.all(
-        map.node_groups.map(async node => {
+        map.categories.map(async category => {
 
-            const currentNode = await getNodeTitle(node.node)
-            let nextNodes = []
+            const categoryTitle = await getCategoryTitle(category.category)
+            category.slug = [category.order, categoryTitle]
+                .filter(element => element != null)
+                .join(' - ')
 
-            // get all nodes names and concat
-            if (node.next_nodes){
-                for (let nextNode of node.next_nodes) {
-                    const nodeTitle = await getNodeTitle(nextNode)
-                    if (nodeTitle) {
-                        nextNodes.push(nodeTitle)
+            if (category.node_groups && category.node_groups.length) {
+                category.node_groups.map(async node => {
+
+                    const currentNode = await getNodeTitle(node.node)
+                    let nextNodes = []
+
+                    // get all nodes names and concat
+                    if (node.next_nodes) {
+                        for (let nextNode of node.next_nodes) {
+                            const nodeTitle = await getNodeTitle(nextNode)
+                            if (nodeTitle) {
+                                nextNodes.push(nodeTitle)
+                            }
+                        }
                     }
-                }
-            }
-            const nextNode = nextNodes.join(', ')
+                    const nextNode = nextNodes.join(', ')
 
-            node.slug = [currentNode, nextNode]
-                .filter(element => element && element.length) // filter out null fields
-                .join(': ')
+                    node.slug = [currentNode, nextNode]
+                        .filter(element => element && element.length) // filter out null fields
+                        .join(': ')
+
+                })
+            }
         })
+
     )
     return map
 }
