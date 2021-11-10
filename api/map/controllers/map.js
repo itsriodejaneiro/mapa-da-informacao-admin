04db7d468100fc0d78a1565582cefca29a78da59
vitusalis <victorsalles1997@gmail.com>
2021-11-10 11:05:43 -0300

    Enable Map draft and Drafts API

--- api/map/controllers/map.js
+++ api/map/controllers/map.js
@@ -1,8 +1,29 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async drafts(ctx) {
+    let entities = await strapi.query('map').model.find();
+
+    // filter out published ones
+    entities = entities.filter(map => map.published_at == null)
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.map }));
+  },
+
+  async draft(ctx) {
+    const { id } = ctx.params;
+
+    let entities = await strapi.query('map').model.find();
+
+    const entity = entities.filter(map => map.id == id)[0]
+
+    return sanitizeEntity(entity, { model: strapi.models.map });
+  },
+};
