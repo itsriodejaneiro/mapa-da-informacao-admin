3db901c8b6f6331c65f2dba29a85c55840035480
vitusalis <victorsalles1997@gmail.com>
2022-01-13 10:56:51 -0300

    Update read_env

--- mapa_da_informacao_api/settings.py
+++ mapa_da_informacao_api/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(BASE_DIR / '.env')
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
