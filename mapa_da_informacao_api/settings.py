6902ccb55cd680b8c735158d2045de4f840b31f7
vitusalis <victorsalles1997@gmail.com>
2022-01-25 09:04:25 -0300

    CSRF_TRUSTED_ORIGINS

--- mapa_da_informacao_api/settings.py
+++ mapa_da_informacao_api/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*').split(',')
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS', default='*').split(',')
 
+CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS', default='*').split(',')
+
 # Application definition
 
 INSTALLED_APPS = [
